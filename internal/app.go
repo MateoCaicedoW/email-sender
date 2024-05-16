9e460f377af1e1ad9db64cf2b704c52b5740ba50
tatang26 <wgarcia@wawand.co>
2024-05-16 17:02:15 -0500

    removing comments

--- internal/app.go
+++ internal/app.go
@@ -18,25 +18,15 @@ var (
 	//go:embed **/*.html **/*.html *.html
 	tmpls embed.FS
 
-	// Assets is the manager for the public assets
-	// it allows to watch for changes and reload the assets
-	// when changes are made.
 	Assets = assets.NewManager(public.Files)
 
-	// TailoOptions allow to define how to compile
-	// the tailwind css files, which is the input and
-	// what will be the output.
 	TailoOptions = []tailo.Option{
 		tailo.UseInputPath("internal/assets/application.css"),
 		tailo.UseOutputPath("public/application.css"),
 		tailo.UseConfigPath("tailwind.config.js"),
 	}
 
-	// GlovesOptions are the options that will be used by the gloves
-	// tool to hot reload the application.
 	GlovesOptions = []gloves.Option{
-		// Run the tailo watcher so when changes are made to
-		// the html code it rebuilds css.
 		gloves.WithRunner(tailo.WatcherFn(TailoOptions...)),
 		gloves.WithRunner(Assets.Watch),
 		gloves.WatchExtension(".go", ".css", ".js", ".html"),
@@ -44,7 +34,6 @@ var (
 )
 
 func init() {
-	//register custom type decoder functions for time.Time
 	form.RegisterCustomTypeFunc(DecodeTime, time.Time{})
 }
 
